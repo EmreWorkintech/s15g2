b9cbe14e03c26e90dcff84c86f27452fbdf85f0c
EmreSahiner <emre@journeyapp.com>
2023-03-07 11:18:08 +0300

    Derste beraber yaptıklarımız

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,40 @@
 // AUTHENTICATION
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../../config/index')
+
 const restricted = (req, res, next) => {
-  next()
+  const token = req.headers.authorization;
+  // token olmayabilir
+  if(token){
+    
+    jwt.verify(token, JWT_SECRET, (err,decodedJWT)=> {
+
+      if(err) {
+        //token var, geçersiz
+        next({status:401, message: "geçersiz token!..."});
+      } else {
+        // token var, geçerli
+        req.userInfo = decodedJWT;
+        next();
+      }
+
+    })
+  } else {
+    next({status: 401, message: "Hop. token nerede?"})
+  }
+
+  
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role => (req, res, next) => {  //currying
+
+  if(req.userInfo && req.userInfo.role === role) {
+    next()
+  } else {
+    next({status:403, message: "Yetkiniz yok!.."})
+  }
+  
 }
 
 module.exports = {
